d390114aa33188b63129e5a5ef7b371d8872efa9
shimahi <shimahi@reah.jp>
2024-08-09 22:10:44 +0900

    文字組み周りの設定

--- panda.config.ts
+++ panda.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from '@pandacss/dev'
+import { defineConfig, defineRecipe } from '@pandacss/dev'
 
 export default defineConfig({
   jsxFramework: 'qwik',
@@ -6,9 +6,80 @@ export default defineConfig({
   outdir: 'src/styled-system',
   include: ['./src/**/*.{js,jsx,ts,tsx}'],
   exclude: [],
-  globalCss: {},
+  globalCss: {
+    '*': {
+      fontFamily:
+        '"kanit", "Noto Sans JP", "Hiragino Kaku Gothic ProN", Meiryo, sans-serif',
+      whiteSpace: 'normal',
+    },
+  },
   theme: {
-    extend: {},
+    extend: {
+      textStyles: {
+        heading: {
+          description: 'ページの見出しの文字',
+          value: {
+            fontWeight: '500',
+            fontSize: '32px',
+            lineHeight: 1.46,
+            letterSpacing: '0.05em',
+            textDecoration: 'None',
+            textTransform: 'None',
+          },
+        },
+        button: {
+          description: 'インタラクティブ要素全般に使用する文字',
+          value: {
+            fontSize: '16px',
+            fontWeight: '700',
+            lineHeight: '20.27px',
+            letterSpacing: '0.05em',
+            textDecoration: 'None',
+            textTransform: 'None',
+          },
+        },
+        input: {
+          description: 'フォームの入力欄の文字',
+          value: {
+            fontSize: '16px',
+            fontWeight: '400',
+            lineHeight: '18px',
+            letterSpacing: '0.05em',
+          },
+        },
+        body: {
+          description: '記事の文章などに用いる',
+          value: {
+            fontSize: '14px',
+            fontWeight: '400',
+            lineHeight: '18px',
+            letterSpacing: '0.05em',
+          },
+        },
+        subtitle1: {
+          description: '大きめのサブタイトルの文字',
+          value: {
+            fontSize: '16px',
+            fontWeight: '600',
+            lineHeight: '23.17px',
+            letterSpacing: '0.05em',
+            textDecoration: 'None',
+            textTransform: 'None',
+          },
+        },
+        caption: {
+          description: '日付など、補足に用いる小さい文字',
+          value: {
+            fontSize: '12px',
+            fontWeight: '400',
+            lineHeight: '17.94px',
+            letterSpacing: '0.05em',
+            textDecoration: 'None',
+            textTransform: 'None',
+          },
+        },
+      },
+    },
   },
   utilities: {},
 })
