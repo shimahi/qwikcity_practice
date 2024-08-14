7e5c68f7031a52d4aea71b7403f5d743764a8c8a
shimahi <shimahi@reah.jp>
2024-08-14 22:08:59 +0900

    アイコンコンポーネントの作成

---
+++ src/components/ui/svg/index.ts
@@ -0,0 +1,11 @@
+import { Check } from './Check'
+import { Close } from './Close'
+import { Menu } from './Menu'
+import { Pencil } from './Pencil'
+
+export const SVG = {
+  Menu,
+  Close,
+  Pencil,
+  Check,
+}
